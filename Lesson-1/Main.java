c0716cc4e4fc5adf71fb1077af9e8894c608da48
vaidehi <vaidehi117v@gmail.com>
2026-05-06 17:19:57 -0400

    object, new and class

--- Lesson-1/Main.java
+++ Lesson-1/Main.java
@@ -1,18 +1,35 @@
- 
- class AddNumbers{
-    int a;
-    int b;
+// Example one 
+//  class AddNumbers{
+//     int a;
+//     int b;
 
-    void add(){
-       int sum = a + b;
-       System.out.println("Sum: " + sum);
-    }
- }
- public class Main {
-    public static void main(String[] args){
-        AddNumbers obj = new AddNumbers();
-        obj.a = 10;
-        obj.b = 20;
-        obj.add();
-    }
- }
+//     void add(){
+//        int sum = a + b;
+//        System.out.println("Sum: " + sum);
+//     }
+//  }
+//  public class Main {
+//     public static void main(String[] args){
+//         AddNumbers obj = new AddNumbers();
+//         obj.a = 10;
+//         obj.b = 20;
+//         obj.add();
+
+//     }
+//  }
+
+// Example two
+class Square{
+   int num;
+   void calculateSquare(){
+      int result = num * num;
+      System.out.println("Square is: " + result);
+   }
+}
+public class Main {
+   public static void main(String[] args){
+      Square s = new Square();
+      s.num = 6;
+      s.calculateSquare();
+   }
+}

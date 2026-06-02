2f0fae589b95cb2e3b4712fb9f36347cc8900a89
vaidehi <vaidehi117v@gmail.com>
2026-06-02 15:13:30 -0400

    lesson-2 incomplete

--- Lesson-2/Main.java
+++ Lesson-2/Main.java
@@ -1,1 +1,40 @@
 
+// Example 1
+
+// class Student {
+//     String name;
+//     int age;
+// }
+
+// public class Main {
+//     public static void main(String[] args){
+//         Student s1 = new Student();
+
+//         s1.name = "Alice";
+//         s1.age = 20;
+//         System.out.println(s1.name + " " + s1.age);
+//     }
+// }
+
+// Example 2
+
+// class Student {
+//     String name;
+//     int age;
+
+//     Student(){
+//         name = "Vaidehi";
+//         age = 26;
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args){
+//         Student s1 = new Student();
+
+//         System.out.println(s1.name + " " + s1.age);
+//     }
+// }
+
+// Example 3
+
